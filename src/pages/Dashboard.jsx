35054425f70b8004730c1c60dbf8fd2af8a765c1
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-23 19:16:10 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -472,6 +472,7 @@ export default function Dashboard() {
             migrationTasks={migrationTasks}
             homologationTasks={homologationTasks}
             risks={risks}
+            products={products}
           />
         </div>
       )}
