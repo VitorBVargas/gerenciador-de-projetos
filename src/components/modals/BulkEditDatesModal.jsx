83d24c035461b72f1f5268b556d9a62d5cb4b78b
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-23 21:08:57 +0000

    File changes

--- src/components/modals/BulkEditDatesModal.jsx
+++ src/components/modals/BulkEditDatesModal.jsx
@@ -89,13 +89,6 @@ export default function BulkEditDatesModal({
   const handleConfirm = async () => {
     const eventsToUpdate = Object.values(editedEvents).map(event => {
       const originalEvent = timelineEvents.find(e => e.id === event.id);
-      if (applyToAll) {
-        return {
-          ...originalEvent,
-          start_date: event.start_date || originalEvent.start_date,
-          end_date: event.end_date || originalEvent.end_date
-        };
-      }
       return {
         ...originalEvent,
         start_date: event.start_date || originalEvent.start_date,
