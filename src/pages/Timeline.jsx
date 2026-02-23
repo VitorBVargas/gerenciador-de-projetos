0caab069657b23c603ed45bc77b24f4ca6377ffb
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-23 20:28:24 +0000

    File changes

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -197,10 +197,7 @@ export default function Timeline() {
     return Math.round(productProgresses.reduce((a, b) => a + b, 0) / productProgresses.length);
   };
 
-  // Reset product index when vertical changes
-  React.useEffect(() => {
-    setCurrentProductIndex(0);
-  }, [activeVertical]);
+
 
   return (
     <div className="p-6 lg:p-8 space-y-6">
