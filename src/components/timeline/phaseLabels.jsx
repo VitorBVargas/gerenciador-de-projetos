088904f0566a56764a34da7329eb494cbe90a38d
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-23 20:44:42 +0000

    File changes

---
+++ src/components/timeline/phaseLabels.jsx
@@ -0,0 +1,14 @@
+export const phaseLabels = {
+  planejamento_contrato: 'Planejamento/Contrato',
+  kickoff: 'Kickoff',
+  diagnostico: 'Diagnóstico',
+  onboarding_cliente: 'Onboarding Cliente',
+  configuracao_migracao_hml: 'Configuração/Migração de Homologação',
+  homologacao_base: 'Homologação da Base',
+  migracao_prd_blackout: 'Migração de PRD (Blackout)',
+  configuracao_prd: 'Configuração de PRD',
+  treinamento: 'Treinamento',
+  go_live: 'Go-Live',
+  operacao_assistida: 'Operação Assistida',
+  encerramento_bastao: 'Encerramento/Passagem de Bastão'
+};
