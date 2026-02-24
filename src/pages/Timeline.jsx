ecd0eb930028bfac308d421475b782a95a882802
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-24 00:45:16 +0000

    File changes

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -70,6 +70,8 @@ export default function Timeline() {
     queryFn: () => base44.entities.Project.list('-created_date')
   });
 
+  const activeProject = projects.find(p => p.id === projectId);
+
   const { data: products = [] } = useQuery({
     queryKey: ['products', projectId],
     queryFn: () => projectId ? base44.entities.Product.filter({ project_id: projectId }) : [],
