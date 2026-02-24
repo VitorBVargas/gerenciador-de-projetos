855b1cbd1d13862f57958a22393d2a0089aab2b4
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-24 00:45:36 +0000

    File changes

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -84,7 +84,7 @@ export default function Timeline() {
     enabled: !!projectId
   });
 
-  const activeProject = projects.find(p => p.id === projectId);
+  const schedulingType = activeProject?.scheduling_type || 'por_produto';
 
   const createMutation = useMutation({
     mutationFn: (data) => base44.entities.TimelineEvent.create(data),
