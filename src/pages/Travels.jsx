62c0b28951dcc9dce35f31d33e3247a6a6dae6d7
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-25 11:33:43 +0000

    File changes

--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -473,8 +473,7 @@ export default function Travels() {
                         ))}
                       </tbody>
                     </table>
-                  </div>
-              </div>
+                </div>
             </div>
           )}
         </>
