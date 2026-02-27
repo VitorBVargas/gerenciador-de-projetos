3779f7ba481a7f2018ee27c9893327629ac6f76b
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-27 14:19:17 +0000

    File changes

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -479,6 +479,7 @@ export default function ProjectsList() {
         open={crmImportModalOpen}
         onOpenChange={setCrmImportModalOpen}
         onSuccess={handleImportSuccess}
+        portfolioFilter={portfolioFilter}
       />
 
       {/* Setup Wizard (after CRM import) */}
