4bf9bcc7aabca6e21ad9b9a2b61009c220b8977a
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-02-27 14:17:41 +0000

    File changes

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -303,7 +303,7 @@ export default function ProjectsList() {
         {/* Header */}
         <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
           <div>
-            <h1 className="text-3xl font-bold text-white">Portifólio SC/MG</h1>
+            <h1 className="text-3xl font-bold text-white">Portfólio {portfolioLabels[portfolioFilter]}</h1>
             <p className="text-slate-400 mt-1">{activeProjects.length} projeto(s) ativo(s)</p>
           </div>
           <div className="flex gap-3">
