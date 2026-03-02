6281cc6ac056ddaf43a6035bc5e5e0cba291c5fb
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-03-02 11:03:49 +0000

    File changes

--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -41,6 +41,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 
 import {
   AlertDialog,
