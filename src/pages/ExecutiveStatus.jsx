2f5c3e467a064a68ab7073cc1be265b660a8c726
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-03-10 14:18:49 +0000

    File changes

--- src/pages/ExecutiveStatus.jsx
+++ src/pages/ExecutiveStatus.jsx
@@ -917,7 +917,7 @@ export default function ExecutiveStatus() {
                       </div>
                     )}
                     {(() => {
-                      const projectEvents = allTimelineEvents.filter(e => e.project_id === project.id);
+                      const projectEvents = getProjectEvents(project);
                       const latestDate = projectEvents.length > 0 
                         ? new Date(Math.max(...projectEvents.map(e => e.end_date ? new Date(e.end_date).getTime() : 0)))
                         : null;
