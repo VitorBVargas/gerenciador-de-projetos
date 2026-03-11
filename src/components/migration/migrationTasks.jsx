9f8c98466200b1434e2bb8f1235b7213d5ce4860
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-03-11 15:27:56 +0000

    File changes

--- src/components/migration/migrationTasks.jsx
+++ src/components/migration/migrationTasks.jsx
@@ -859,6 +859,33 @@ export const migrationTasksByProduct = {
         'Extrair Pessoal Bolsa Estudos'
       ]
     }
+  ],
+  'RH (Cloud)': [
+    {
+      section: 'Dados Cadastrais',
+      tasks: [
+        'Extrair Distância',
+        'Extrair Região',
+        'Extrair Natureza Diária',
+        'Extrair Diária',
+        'Extrair Cursos',
+        'Extrair Tipos Cursos',
+        'Extrair Instituições',
+        'Extrair Pessoas',
+        'Extrair Pessoal Cursos',
+        'Extrair Pessoal Filiação',
+        'Extrair Pessoal Documento',
+        'Extrair Pessoal Endereço',
+        'Extrair Pessoal Email',
+        'Extrair Pessoal Telefone',
+        'Extrair Pessoal Dependente',
+        'Extrair Pessoal Experiência',
+        'Extrair Pessoal Averbação',
+        'Extrair Pessoal Conta Bancária',
+        'Extrair Pessoal Cota Sindical',
+        'Extrair Pessoal Bolsa Estudos'
+      ]
+    }
   ]
 };
 
