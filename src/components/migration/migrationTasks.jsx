6373f20c309e1b6b967b8f407fe36fd99c97287b
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-03-12 14:13:11 +0000

    Manual code change

--- src/components/migration/migrationTasks.jsx
+++ src/components/migration/migrationTasks.jsx
@@ -1284,7 +1284,7 @@ export const migrationTasksByProduct = {
     'Extrair Registro de Ocorrências',
     'Extrair Relatórios de Reconhecimento'
   ]),
-  'Patrimônio': parseTasksIntoSections([
+  'patrimonio': parseTasksIntoSections([
     'Dados Cadastrais',
     'Extrair Configuração Organograma',
     'Extrair Parâmetro Exercício',
