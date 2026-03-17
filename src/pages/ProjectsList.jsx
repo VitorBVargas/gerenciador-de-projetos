f9ddb6f00b71dda7a83c9c55b370ba82522446d1
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-03-17 12:13:12 +0000

    File changes

--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -44,8 +44,8 @@ const statusLabels = {
 };
 
 const portfolioLabels = {
-  grandes_contas_sc_mg: 'Grande Contas SC/MG',
-  grandes_contas_sc_sp: 'Grande Contas SC/SP',
+  grandes_contas_sc_mg: 'Grandes Contas SC/MG',
+  grandes_contas_sc_sp: 'Grandes Contas SC/SP',
   medias_contas: 'Médias Contas',
 };
 
