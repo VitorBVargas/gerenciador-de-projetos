d5ee4f70876513e1c3b049425320f84f453953e8
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-04-06 12:11:25 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import Activities from './pages/Activities';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -43,11 +44,16 @@ const AuthenticatedApp = () => {
   }
 
   // Render the main app
-  return (
+ return (
     <Routes>
       <Route path="/" element={
         <LayoutWrapper currentPageName={mainPageKey}>
           <MainPage />
+        </LayoutWrapper>
+      } />
+      <Route path="/Activities" element={
+        <LayoutWrapper currentPageName="Activities">
+          <Activities />
         </LayoutWrapper>
       } />
       {Object.entries(Pages).map(([path, Page]) => (
