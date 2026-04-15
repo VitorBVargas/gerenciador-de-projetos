793417d1922d369e3993ba53e174b3ce59e9b566
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-04-15 12:04:27 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import Activities from './pages/Activities';
+import PendenciaEdital from './pages/PendenciaEdital';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -54,6 +55,11 @@ const AuthenticatedApp = () => {
       <Route path="/Activities" element={
         <LayoutWrapper currentPageName="Activities">
           <Activities />
+        </LayoutWrapper>
+      } />
+      <Route path="/PendenciaEdital" element={
+        <LayoutWrapper currentPageName="PendenciaEdital">
+          <PendenciaEdital />
         </LayoutWrapper>
       } />
       {Object.entries(Pages).map(([path, Page]) => (
