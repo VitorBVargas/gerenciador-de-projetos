34d7e06355be40ac493a3debf19465f1c20b4ea5
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-04-15 12:11:27 +0000

    Manual code change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
         </div>
 
         {/* Button Grid */}
-        <div className="grid md:grid-cols-3 gap-6 md:gap-4 max-w-4xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
           {/* Projetos Internos */}
           <button
             onClick={() => handleNavigation('InternalProjectsList')}
