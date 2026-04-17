c1e149411280c6f495494763403921fab26f0227
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-04-17 13:47:30 +0000

    Manual code change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -33,6 +33,7 @@ const navigation = [
   { name: 'Atividades', href: 'Activities', icon: CheckSquare },
   { name: 'Viagens', href: 'Travels', icon: Plane },
   { name: 'Orçamento', href: 'Budget', icon: DollarSign },
+  { name: 'Apontamento de Horas', href: 'HorasApontamento', icon: Timer },
   { name: 'IA Projetos Betha', href: 'StatusReports', icon: FileText },
   { name: 'KPI / Indicadores', href: 'Reports', icon: FileText },
   //{ name: 'Relatórios', href: 'Reports', icon: FileText },
