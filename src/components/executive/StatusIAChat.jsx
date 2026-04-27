c7c5c321647cd447500a5bc31e2c9b1997b34f74
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-04-27 15:15:30 +0000

    File changes

--- src/components/executive/StatusIAChat.jsx
+++ src/components/executive/StatusIAChat.jsx
@@ -34,7 +34,7 @@ function buildSuggestedPrompts({ projects, statusSummary, timelineSummary, finan
 
   return [
     'Quais as proximas datas de conclusão?',
-    'Me fale os valores financeiros de implantação e inclusão dos proximos 3 meses, separadamente',
+    'Me fale os valores financeiros de implantação e inclusão dos proximos 3 meses por projeto',
     variablePrompt,
   ];
 }
