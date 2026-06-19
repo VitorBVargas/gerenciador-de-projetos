1f3aaf5078ac82a0af834f0cdaba9cc65e3402aa
Betha Sistemas - 00456865000167 <vitor.vargas@betha.com.br>
2026-06-19 13:02:44 +0000

    Manual code change

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 const TOAST_LIMIT = 20;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 5000;
 const DEFAULT_TOAST_DURATION = 5000;
 
 const actionTypes = {
